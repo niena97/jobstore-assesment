459e9da5f77dec55bb7cbb4893a8f11a320349cc
Siti Nur Amalina Ghazali <stnrmln@gmail.com>
2022-11-07 01:11:31 +0800

    initial commit

---
+++ src/App.vue
@@ -0,0 +1,20 @@
+<template>
+  <HelloWorld/>
+</template>
+
+<script>
+import HelloWorld from './components/HelloWorld.vue'
+
+export default {
+  name: 'App',
+  components: {
+    HelloWorld
+  }
+}
+</script>
+
+<style>
+#app {
+  margin-top: 0px;
+}
+</style>
---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,304 @@
+<template>
+  <div class="div-1">
+    <nav class="navbar navbar-expand-lg">
+      <div class="container-fluid">
+        <a class="navbar-brand" href="#"><img src="https://asset.jobstore.com/images/branding/jobstore_logo_white.png"
+            style="width: 50%;"></a>
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent"
+          aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+          <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarSupportedContent">
+          <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+            <li class="nav-item">
+              <a class="nav-link active" aria-current="page" href="#" style="color: white;">SEARCH JOBS</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="#">COMPANIES</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="#">RESOURCES</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="#">BLOG</a>
+            </li>
+            <li class="nav-item dropdown" style="margin-left:450px;">
+              <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown"
+                aria-expanded="false">
+                <img src="//asset.jobstore.com/images/index_icons/icon_flag_my.png">
+              </a>
+              <ul class="dropdown-menu dropdown-menu-dark">
+                <li><a class="dropdown-item" href="#"><img
+                      src="https://asset.jobstore.com/images/index_icons/icon_flag_au.png"> AUSTRALIA</a></li>
+                <li><a class="dropdown-item" href="#"><img
+                      src="https://asset.jobstore.com/images/index_icons/icon_flag_sg.png"> SINGAPORE</a></li>
+              </ul>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="#">SIGN IN</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="#">SIGN UP</a>
+            </li>
+            <li class="nav-item">
+              <button class="btn btn-sm" type="button"
+                style="border-radius: 25px; width: 150%; background-color: #0732a2; color: white;">EMPLOYERS</button>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </nav>
+
+    <div class="container-box" style="margin-top: 40px;">
+      <div class="grid-container">
+        <div class="item1" style="margin-right: 80px;">
+          <div style="color: white;">
+            <h1 style="font-weight: 700;
+                font-family: 'Fira Sans', sans-serif; font-size: 37px;">MALAYSIA'S <span class="text-orange-color">NO.
+                1</span> JOB SITE
+            </h1>
+            <p>More than <span class="text-orange-color">16,104</span> jobs available across Malaysia, worldwide</p>
+            <div style="margin: 0 auto; display: block;">
+              <button type="button" class="btn btn-warning btn-lg"
+                style="padding: 15px 60px; color: white; float: left;"><b>FREE SIGN UP</b></button>
+              <button type="button" class="btn btn-primary btn-lg"
+                style="padding: 15px 60px; background-color: #0732a2; color: white; float: right;"><b>SEARCH
+                  JOBS</b></button>
+            </div>
+          </div>
+          <div class="row" style="padding-top: 20px; width:400px; margin: auto;">
+            <div class="col">
+              <div class="card border-0" style="height: 100%;">
+                <img
+                  src="https://jhp-blog.s3-accelerate.amazonaws.com/magazine/wp-content/uploads/2020/09/Win-Friends-Influence-People-at-Your-Workplace.jpeg"
+                  class="card-img-top">
+                <div class="card-body">
+                  <p class="card-text"><b>TRENDING ARTICLES</b><br>
+                    4 Principles on How to Win Friends & Influence People at Your Workplace<br>
+                    <a href="#">READ MORE</a>
+                  </p>
+                </div>
+              </div>
+            </div>
+          </div>
+
+        </div>
+        <div class="item2">
+          <img src="https://asset.jobstore.com/images/index/index_mobile_qr.png" style="height: 325px;">
+        </div>
+        <div class="item3">
+          <div style="background-color: #EEEEEE; opacity:0.9; width: 100%; padding: 10px;">
+            <b>FEATURED EMPLOYERS</b> <br>
+            <img
+              src="https://asset.jobstore.com/uploads/image/755470/oxkikZNLjI/379005_410686465665575_1676421468_n.jpg"
+              style="width: 50px;">&nbsp;&nbsp;
+            <img src="https://asset.jobstore.com/uploads/image/10/35WRDoEIED/JobstoreLinkedinLogo.png"
+              style="width: 50px;"> <br>
+            <a href="#">MORE</a>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+
+  <div class="container-box">
+    <img src="//asset.jobstore.com/images/index/index_jobseeker_invite.gif"
+      style="display: block; width: 100%; margin: auto; margin-top: 70px;">
+    <h2 style="margin-top: 70px;"><b>FEATURED EMPLOYERS</b></h2>
+    <p><a href="#">view more</a></p>
+    <div class="row">
+      <div class="col-sm-6">
+        <div class="card" style="height: 100%;">
+          <img
+            src="//asset.jobstore.com/uploads/image/755470/kfAdCmIDwC/42132806_1997376280330990_4505345935303245824_n11.png"
+            class="card-img-top">
+          <div class="card-body">
+            <h5 class="card-title">AEON Credit Service (M) Berhad</h5>
+            <p class="card-text">No. 8, Level 18, UOA Corporate Tower, Avenue 10, The Vertical Bangsar South City, Jalan
+              Kerinchi, 59200 Kuala Lumpur</p>
+            <a href="#" class="btn btn-primary">Apply Jobs</a>
+          </div>
+        </div>
+      </div>
+      <div class="col-sm-6">
+        <div class="card" style="height: 100%;">
+          <img src="//asset.jobstore.com/uploads/image/10/MJ0UVmovN6/jobstore banner.jpg" class="card-img-top">
+          <div class="card-body">
+            <h5 class="card-title">JOBSTORE GROUP BERHAD</h5>
+            <p class="card-text">Mid Valley City, 58000 Kuala Lumpur, Wilayah Persekutuan Kuala Lumpur, Malaysia</p>
+            <a href="#" class="btn btn-primary">Apply Jobs</a>
+          </div>
+        </div>
+      </div>
+    </div>
+    <h2 style="margin-top: 70px;"><b>TRENDING JOBS IN MALAYSIA</b></h2>
+    <p><a href="#">view more</a></p>
+    <div class="row" style="text-align: left;">
+      <div class="col-sm-4">
+        <ul class="list-group list-group-flush">
+          <li class="list-group-item"><b style="color: #0704e1">Key Account Executive - Sales</b> <br> Klang</li>
+          <li class="list-group-item"><b style="color: #0704e1">SALES & PROPOSAL ENGINEER</b> <br> Telok Panglima Garang
+          </li>
+          <li class="list-group-item"><b style="color: #0704e1">Accounts & Finance Executive</b> <br> Sri Petaling</li>
+          <li class="list-group-item"><b style="color: #0704e1">Sales Executive - Based in KL</b> <br> Kuala Lumpur</li>
+        </ul>
+      </div>
+      <div class="col-sm-4">
+        <ul class="list-group list-group-flush">
+          <li class="list-group-item"><b style="color: #0704e1">Costing Executive</b> <br> Klang</li>
+          <li class="list-group-item"><b style="color: #0704e1">Financial Planner</b> <br> Perak</li>
+          <li class="list-group-item"><b style="color: #0704e1">Safety & Health Officer</b> <br> Kuala Lumpur</li>
+          <li class="list-group-item"><b style="color: #0704e1">Telesales</b> <br> Kota Damansara</li>
+          <li class="list-group-item"><b style="color: #0704e1">Sales Executive</b> <br> Shah Alam</li>
+        </ul>
+      </div>
+      <div class="col-sm-4">
+        <ul class="list-group list-group-flush">
+          <li class="list-group-item"><b style="color: #0704e1">Architecture Supervisor</b> <br> Kuala Lumpur</li>
+          <li class="list-group-item"><b style="color: #0704e1">M&E Engineer</b> <br> Kuala Lumpur</li>
+          <li class="list-group-item"><b style="color: #0704e1">Civil & Structure Engineer</b> <br> Kuala Lumpur</li>
+          <li class="list-group-item"><b style="color: #0704e1">Sales Coordinator</b> <br> Mid Valley City</li>
+        </ul>
+      </div>
+    </div>
+    <p style="margin-top: 70px; color: grey;"><b>IN JUST ONE MINUTE</b></p>
+    <h1><b>SEARCH BY POPULAR CATEGORIES</b></h1>
+    <form class="d-flex" role="search">
+      <input class="form-control me-2 shadow p-3 mb-5 bg-white rounded" type="search"
+        placeholder="Search by keyword or job title" aria-label="Search">
+      <!-- <button class="btn btn-outline-success" type="submit">Search</button> -->
+    </form>
+    <p>Popular Categories &nbsp;<span><a href="#">Sales & Marketing Jobs</a>&nbsp;&nbsp;
+        <a href="#">Information Technology Jobs</a>&nbsp;&nbsp;
+        <a href="#">Accounting Auditing Jobs</a></span></p>
+  </div>
+
+  <div style="width: 100%; background-color: #f8f8f8; margin-top: 80px;">
+    <div class="container-box">
+      <img src="https://asset.jobstore.com/images/branding/jobstore_logo_blue.png"
+        style="margin-top: 50px; width: 23%;">
+      <div class="row" style="justify-content: space-evenly; margin-top: 70px; text-align: left;">
+        <div class="col-sm-2">
+          <b style="color: #0704e1">COMPANY</b>
+          <div>
+            <a href="#">About Us</a> <br> <a href="#">Contact Support</a> <br> <a href="#">Careers @ Jobstore</a> <br>
+            <a href="#">Blog</a> <br> <a href="#">Partner Sites</a> <br> <a href="#">Fulfillment Policy</a>
+          </div>
+        </div>
+        <div class="col-sm-2">
+          <b style="color: #0704e1">JOB SEEKER</b>
+          <div>
+            <a href="#">Sign Up</a> <br> <a href="#">Sign In</a> <br> <a href="#">Check Applications</a> <br> <a
+              href="#">Jobseeker FAQs</a> <br> <a href="#">Browse Jobs</a> <br> <a href="#">Browse by Industry</a>
+          </div>
+        </div>
+        <div class="col-sm-2">
+          <b style="color: #0704e1">EMPLOYER</b>
+          <div>
+            <a href="#">Create Account</a> <br> <a href="#">Post Jobs</a> <br> <a href="#">Products & Prices</a> <br> <a
+              href="#">Customer Support</a> <br> <a href="#">Contact Sales</a> <br> <a href="#">Facebook Tab</a>
+          </div>
+        </div>
+        <div class="col-sm-4">
+          <b style="color: #0704e1">CONNECT WITH US</b>
+          <div>
+            <a href="#">Facebook</a> <br> <a href="#">Twitter</a> <br> <a href="#">LinkedIn</a> <br> <a
+              href="#">Instagram</a> <br> <a href="#">Call us at +603 2716 5199</a> <br> <a href="#">Send us a
+              message</a>
+          </div>
+        </div>
+      </div>
+      <p style="margin-top: 30px; font-size: 0.8rem;">© 2022 Jobstore • Terms of Service • Privacy Policy • All Rights
+        Reserved</p>
+      <hr>
+      <p style=" text-align: justify; text-justify: inter-word; font-size: 0.8rem;">Jobstore is the biggest job
+        distribution and job search
+        platform with a broad presence in Malaysia, Australia,
+        Hong Kong, Singapore, the Philippines, and Indonesia.<br><br>
+
+
+        It is currently one of the fastest growing job portals in the Asia Pacific region — recognized with multiple
+        awards such as Top 100 Private Technology Company, Top 10 Consumer Cloud Application, etc. Having more than
+        20,000
+        top firms and a growing number of career opportunities on board, Jobstore connects the right people with the
+        right
+        jobs, serving tens of thousands of employers of all sizes and industries.<br><br>
+
+
+        Furthermore, our social networking search technology is capable of tracking users' online behavior, thus
+        ensuring
+        that jobseekers are presented with the most recent and relevant jobs from their search.<br><br>
+
+
+        Jobstore has also streamlined the hiring process radically, making it much more organized and efficient. With
+        just
+        one submission, employers and recruiters are able to advertise jobs on multiple job sites, classified ads and
+        social network sites. We also integrate a state-of-the-art application tracking system that will help your
+        recruitment team work more efficiently.<br><br>
+
+
+        Trusted by 8 million users, we refuse to settle and will continue to explore new frontiers across all emerging
+        online channels. We strive to gain new insights and understanding of the job market so that we will help not
+        only
+        employers to fill their vacancies, but also help jobseekers to land the jobs that match their true
+        potential.<br><br>
+
+
+        We believe everyone can, and should love his or her job. And Jobstore is where you'll find your calling.</p>
+    </div>
+  </div>
+</template>
+
+<script>
+
+</script>
+
+<style>
+.grid-container {
+  display: grid;
+  grid-template-columns: 68% 37%;
+  gap: 10px;
+  padding: 10px;
+}
+
+.grid-container>div {
+  text-align: center;
+  padding: 10px 0;
+}
+
+.item1 {
+  grid-row: 1 / 3;
+}
+
+.div-1 {
+  background-image: linear-gradient(rgba(0, 0, 0, 0.8), rgba(0, 0, 0, 0.85)), url('https://asset.jobstore.com/images/index/index_people.png');
+  height: 100vh;
+  width: 100%;
+}
+
+.text-orange-color {
+  color: #ffa800;
+}
+
+.nav-item .nav-link {
+  color: white;
+}
+
+.container-box {
+  width: 900px;
+  text-align: center;
+  justify-content: center;
+  align-items: center;
+  display: block;
+  margin: auto;
+}
+
+a:link {
+  text-decoration: none;
+}
+
+a:hover {
+  color: black;
+}
+</style>
+
